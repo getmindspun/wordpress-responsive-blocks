e45d9adf2e4c8061e38281ced1039550330d638a
Matthew R. Laue <mattlaue@gmail.com>
2023-12-07 09:02:24 -0800

    Update the heading icon color.

--- packages/block-library/src/heading/index.ts
+++ packages/block-library/src/heading/index.ts
@@ -1,8 +1,14 @@
 import {registerBlockType} from '@wordpress/blocks';
+import {heading} from '@wordpress/icons';
 
 import './style.scss';
 import edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
-registerBlockType( metadata.name, { edit, save} as any );
+const icon = {
+    foreground: '#005ffe',
+    src: heading
+}
+
+registerBlockType( metadata.name, { edit, save, icon } as any );
