68bace22a42b6ae9c1112f589777d0e78cef7758
Matthew R. Laue <mattlaue@gmail.com>
2024-03-06 08:49:47 -0800

    mrblx npm improvements.

--- packages/create-block/index.js
+++ packages/create-block/index.js
@@ -1,21 +1,25 @@
-const { join } = require('path');
+const {join} = require('path');
 
 module.exports = {
-	assetsPath: join(__dirname, 'assets'),
-	blockTemplatesPath: join(__dirname, 'templates', 'block'),
-	defaultValues: {
-		attributes: {
-			blockId: {
-				type: 'string',
-			},
-			style: {
-				type: 'object',
-				default: {},
-			},
-		},
-		npmDevDependencies: ['@mindspun/mrblx', 'shx'],
-		customScripts: {
-			postinstall: 'npx shx cp -f assets/webpack.config.js .',
-		},
-	},
+    assetsPath: join(__dirname, 'assets'),
+    blockTemplatesPath: join(__dirname, 'templates', 'block'),
+    defaultValues: {
+        attributes: {
+            blockId: {
+                type: 'string',
+            },
+            style: {
+                type: 'object',
+                default: {},
+            },
+        },
+        npmDevDependencies: [
+            '@mindspun/mrblx',
+            '@wordpress/i18n',
+            'shx'
+        ],
+        customScripts: {
+            postinstall: 'npx shx cp -f assets/webpack.config.js .',
+        },
+    },
 };
