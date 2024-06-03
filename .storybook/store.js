11d463813c7283266435e8ed562cd802cd299841
Matthew R. Laue <mattlaue@gmail.com>
2024-06-03 07:01:06 -0700

    Update the storybook store for setDeviceType/getDeviceType.

--- .storybook/store.js
+++ .storybook/store.js
@@ -1,36 +1,38 @@
-import { createReduxStore, register } from '@wordpress/data';
+import {register, createReduxStore} from '@wordpress/data';
 
-const DEFAULT_STATE = {
-    deviceType: 'Desktop',
-};
+window.wp = window.wp || {};
+if (!window.wp.editor) {
+    const DEFAULT_STATE = {
+        deviceType: 'Desktop'
+    };
 
-const store = createReduxStore( 'core/edit-post', {
-    reducer( state = DEFAULT_STATE, action ) {
-        switch ( action.type ) {
-            case 'SET_DEVICE_TYPE':
-                return {
-                    ...state,
-                    deviceType: action.deviceType
-                };
-        }
-
-        return state;
-    },
-
-    actions: {
-        __experimentalSetPreviewDeviceType(deviceType) {
-            return {
-                type: 'SET_DEVICE_TYPE',
-                deviceType,
-            };
-        }
-    },
-
-    selectors: {
-        __experimentalGetPreviewDeviceType( state ) {
-            return state.deviceType;
+    const store = createReduxStore('core/editor', {
+        reducer(state = DEFAULT_STATE, action) {
+            switch (action.type) {
+                case 'SET_DEVICE_TYPE':
+                    return {
+                        ...state,
+                        deviceType: action.deviceType
+                    };
+                default:
+                    return state;
+            }
         },
-    },
-} );
+        actions: {
+            setDeviceType(deviceType) {
+                return {
+                    type: 'SET_DEVICE_TYPE',
+                    deviceType
+                };
+            },
+        },
+        selectors: {
+            getDeviceType(state) {
+                return state.deviceType
+            }
+        },
+    });
 
-register( store );
+    register(store);
+    window.wp.editor = store;
+}
