87cdfc4f4f2a79afb0b91ba78845f87c56924a8a
Matthew R. Laue <mattlaue@gmail.com>
2024-08-10 09:59:03 -0700

    lint fixes

--- packages/block-library/src/form/types.ts
+++ packages/block-library/src/form/types.ts
@@ -1,23 +1,22 @@
-import {ElementsAttributes} from '~shared/types';
+import { ElementsAttributes } from '~shared/types';
 
 export type Props = {
-    clientId: string;
-    isSelected: boolean;
-    attributes: ElementsAttributes & {
-        method: string;
-        action: string | undefined;
-        encType:
-            | 'application/form-data'
-            | 'application/x-www-form-urlencoded'
-            | 'application/json'
-            | undefined;
-    };
-    setAttributes: (attributes: Partial<Props['attributes']>) => void;
+	clientId: string;
+	isSelected: boolean;
+	attributes: ElementsAttributes & {
+		method: string;
+		action: string | undefined;
+		encType:
+			| 'application/form-data'
+			| 'application/x-www-form-urlencoded'
+			| 'application/json'
+			| undefined;
+	};
+	setAttributes: (attributes: Partial<Props['attributes']>) => void;
 };
 
-
 export type FlashEventDetail = {
-    form: HTMLFormElement;
-    message: string;
-    type?: string;
-}
+	form: HTMLFormElement;
+	message: string;
+	type?: string;
+};
