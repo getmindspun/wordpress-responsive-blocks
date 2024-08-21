b36c557492f7d6e3132c576cbfd86c548e716ebf
Matthew R. Laue <mattlaue@gmail.com>
2024-08-21 11:31:04 -0700

    Improved checkbox implementation.

--- packages/block-library/src/buttons/button/controls/CustomEventControl.tsx
+++ packages/block-library/src/buttons/button/controls/CustomEventControl.tsx
@@ -1,52 +1,52 @@
-import { __ } from '@wordpress/i18n';
+import {__} from '@wordpress/i18n';
 
-import { Props } from '../types';
-import { TextControl } from '@wordpress/components';
+import {Props} from '../types';
+import {TextControl} from '@wordpress/components';
 
 const CustomEventControl = (props: {
-	attributes: Props['attributes'];
-	setAttributes: (attributes: Partial<Props['attributes']>) => void;
+    attributes: Props['attributes'];
+    setAttributes: (attributes: Partial<Props['attributes']>) => void;
 }) => {
-	return (
-		<div className={'mrblx--custom-event-control'}>
-			<TextControl
-				label={__('Custom Event')}
-				value={
-					props.attributes.customEvent.type
-						? props.attributes.customEvent.type
-						: ''
-				}
-				onChange={(type) => {
-					const customEvent = {
-						...props.attributes.customEvent,
-						type: type ? type : undefined,
-					};
-					props.setAttributes({ customEvent });
-				}}
-				help={__(
-					'The custom event type to be emitted when the button is clicked.'
-				)}
-			/>
-			{!!props.attributes.customEvent.type ? (
-				<TextControl
-					label={__('Custom Event Detail')}
-					value={
-						props.attributes.customEvent.detail
-							? props.attributes.customEvent.detail
-							: ''
-					}
-					onChange={(detail) => {
-						const customEvent = {
-							...props.attributes.customEvent,
-							detail: detail ? detail : null,
-						};
-						props.setAttributes({ customEvent });
-					}}
-					help={__('The detail attribute included with the event.')}
-				/>
-			) : null}
-		</div>
-	);
+    return (
+        <div className={'mrblx--custom-event-control'}>
+            <TextControl
+                label={__('Custom Event')}
+                value={
+                    props.attributes.customEvent.type
+                        ? props.attributes.customEvent.type
+                        : ''
+                }
+                onChange={(type) => {
+                    const customEvent = {
+                        ...props.attributes.customEvent,
+                        type: type ? type : 'UNKNOWN',
+                    };
+                    props.setAttributes({customEvent});
+                }}
+                help={__(
+                    'The custom event type to be emitted when the button is clicked.'
+                )}
+            />
+            {!!props.attributes.customEvent.type ? (
+                <TextControl
+                    label={__('Custom Event Detail')}
+                    value={
+                        props.attributes.customEvent.detail
+                            ? props.attributes.customEvent.detail
+                            : ''
+                    }
+                    onChange={(detail) => {
+                        const customEvent = {
+                            ...props.attributes.customEvent,
+                            detail: detail ? detail : null,
+                        };
+                        props.setAttributes({customEvent});
+                    }}
+                    help={__('The detail attribute included with the event.')}
+                />
+            ) : null}
+        </div>
+    );
 };
 
 export default CustomEventControl;
